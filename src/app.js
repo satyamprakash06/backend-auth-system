902c4383878cce0c163eb46e0edd1c665b1122fe
satyamprakash06 <satyam060104@gmail.com>
2026-06-23 17:43:25 +0530

    file upload using multer

--- src/app.js
+++ src/app.js
@@ -1,11 +1,42 @@
 import express from "express"
 import authRoute from "./modules/auth/auth.route.js"
 import cookieParser from "cookie-parser"
+import multer from "multer";
+import ApiResponse from "./common/utils/api-response.js";
+import path from "path"
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
+
+
+
+
+
+
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const ext = path.extname(file.originalname);
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext)
+  }
+})
+
+
+
+const upload = multer({storage});
+
+app.post("/upload", upload.single("files"), (req, res) => {
+    console.log(req.files);
+    ApiResponse.ok(res, "files Uploded");
+})
+
+
 
 app.use("api/auth", authRoute);
 
